72f8fb295f3e43b30cb85eb9ac986a61cfb72eea
Andrew Gray <andrew.aj.gray@gmail.com>
2020-08-01 18:59:39 +0930

    fixed errors in schema file

--- schema.sql
+++ schema.sql
@@ -3,15 +3,10 @@ CREATE DATABASE employees_db;
 
 USE employees_db;
 
-CREATE TABLE employee (
-first_name VARCHAR(30) NOT NULL,
-last_name VARCHAR(30) NOT NULL,
-role_id INT NOT NULL,
-manager_id INT,
+CREATE TABLE department (
+name VARCHAR(30),
 id INT AUTO_INCREMENT,
-PRIMARY KEY (id),
-FOREIGN KEY (role_id),
-FOREIGN KEY (manager_id)
+PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
@@ -20,11 +15,16 @@ salary DECIMAL,
 department_id INT,
 id INT AUTO_INCREMENT,
 PRIMARY KEY (id),
-FOREIGN KEY (department_id)
+FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
-CREATE TABLE department (
-name VARCHAR(30),
+CREATE TABLE employee (
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+role_id INT NOT NULL,
+manager_id INT,
 id INT AUTO_INCREMENT,
-PRIMARY KEY (id)
-)
+PRIMARY KEY (id),
+FOREIGN KEY (role_id) REFERENCES role(id),
+FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
