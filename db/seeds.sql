18119b89836aef9e097ca488fa1f7d643c97ab87
Andrew Gray <andrew.aj.gray@gmail.com>
2020-08-09 00:16:09 +0930

    updated seeds file

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,8 @@
 INSERT INTO department (name)
-VALUES ("Sales"), ("Marketing"), ("Engineering");
+VALUES ("Sales"), ("Marketing"), ("Engineering"), ("Finance");
 
 INSERT INTO role (title, salary, department_id)
-VALUES ("Salesperson", 3.0, 1), ("Marketingperson", 3.1, 2), ("Engineeringperson", 3.2, 3);
+VALUES ("Salesperson", 60000, 1), ("Market Researcher", 70000, 2), ("Engineer", 55000, 3), ("Accountant", 60000, 4);
 
 INSERT INTO employee (first_name, last_name, role_id)
 VALUES ("John", "Smith", 1), ("Billy", "Herrington", 2), ("Elon", "Musk", 3);
